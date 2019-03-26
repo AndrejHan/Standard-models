6c35fe905863206d3a60e37e5c281bc161833845
AndrejHan <39159354+AndrejHan@users.noreply.github.com>
2019-03-26 11:52:59 +0100

    Update Friedel (1979).py

--- Two-phase flow/Friedel (1979).py
+++ Two-phase flow/Friedel (1979).py
@@ -1,6 +1,6 @@
 def Friedel_1979 (Array, rho_l, rho_g, rho_tp, DynVisc_l, DynVisc_g, f_lo, f_go, MassQual, MassFlux_tp, D_int, SurfaceTension, Gconst, TPM_name = 'phi^2_pred_Friedel_1979_lo [-]'):
     '''Calculation of Friedel et al. (1979) model for two phase flow multiplier - loquid only.
-    Inputs are: Array - final table, rho_l - Density of the liquid phase, rho_g - Density of the gas phase, rho_tp - Density of the two phase flow, DynVisc_l - Dynamic viscosity of the liquid, DynVisc_g - Dynamic viscosity of the gas, MassQual - mass quality, MassFlux_tp - Total mass flux, D_int - internal diameter of the tube, SurfaceTension - surface tension of the fluid, Gconst - gravitational constant acceleration.'''    
+    Inputs are: Array - final table, rho_l - Density of the liquid phase [kg/m^3], rho_g - Density of the gas phase [kg/m^3], rho_tp - Density of the two phase flow [kg/m^3], DynVisc_l - Dynamic viscosity of the liquid [Pa*s], DynVisc_g - Dynamic viscosity of the gas [Pa*s], MassQual - mass quality [-], MassFlux_tp - Total mass flux [kg/m^2/s], D_int - internal diameter of the tube [m], SurfaceTension - surface tension of the fluid [N/m], Gconst - gravitational constant acceleration [m/s^2].'''    
   
     FroudeN = MassFlux_tp**2 / (Gconst * D_int * rho_tp**2)
     WeberN = MassFlux_tp**2 * D_int *(SurfaceTension * rho_tp) 
