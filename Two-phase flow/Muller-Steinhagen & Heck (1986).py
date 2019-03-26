82734c2ee602a4514e5cdc1f0d2a528ec7af259f
AndrejHan <39159354+AndrejHan@users.noreply.github.com>
2019-03-26 11:27:56 +0100

    Update Muller-Steinhagen & Heck (1986).py

--- Two-phase flow/Muller-Steinhagen & Heck (1986).py
+++ Two-phase flow/Muller-Steinhagen & Heck (1986).py
@@ -1,5 +1,5 @@
 def MullerSteinhagen_Heck_1986(Array, Chisholm_Y, MassQual, TPM_name = 'phi^2_pred_MullerSteinhagen_Heck_1986_lo [-]'):
-    '''Calculation of Muller-Steinhagen and Heck (1986) model for two phase flow multiplier - liquid.
+    '''Calculation of Muller-Steinhagen and Heck (1986) model for two phase flow multiplier - liquid only.
     Inputs are: Array - final table, Chisholm_Y - Y two pase parameter defined by Chisholm, MassQual - mass quality.'''    
     phi = Chisholm_Y**2 * MassQual**3 + (1 - MassQual)**(1/3) * (1 + 2*MassQual*(Chisholm_Y**2 - 1))
     Array[TPM_name] = phi
