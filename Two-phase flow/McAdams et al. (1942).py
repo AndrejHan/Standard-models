b24475a340353f7fa725d3e8f438fe024e7ad215
AndrejHan <39159354+AndrejHan@users.noreply.github.com>
2019-03-26 11:08:40 +0100

    Create McAdams et al. (1942).py

---
+++ Two-phase flow/McAdams et al. (1942).py
@@ -0,0 +1,17 @@
+def McAdams_1942(Array, MassQual, DynVisc_l, DynVisc_g, MassFlux_tp, D_int, rho_tp,  HM_name = 'Dp/dz_pred_McAdams_1942_tp [Pa/m]'):
+    '''Calculation of MC Adams et al. (1942) model for two phase dynamic viscosity. For the calculation of friction factor is used Fang(2011) correlation (by Fanning).
+    Inputs are: Array - final table, MassQual - mass quality, DynVisc_l - Dynamic viscosity of liquid, DynVisc_g - Dynamic viscosity of gas, MassFlux_tp - two phase mass flux, D_int - internal diameter, rho_tp - two phase flow density.'''
+    import pandas as pd
+    import numpy as np
+    mu_tp =(MassQual/DynVisc_g + (1-MassQual)/DynVisc_l)**-1
+    Re_tp = (MassFlux_tp * D_int)/mu_tp
+        
+    Re_low = Re_tp[Re_tp < 2000] #laminar flow
+    f_low = 16./Re_low
+    Re_high = Re_tp[Re_tp >= 2000]  #turbulent flow, usage of the Fang(2011)
+    f_high = 0.25*0.25*(np.log10(150.39/(Re_high**0.98865) - 152.66/Re_high))**(-2) 
+    f = pd.concat([f_low,f_high])    
+    
+    Dp_dz = (2 * f * MassFlux_tp**2)/(rho_tp * D_int)
+    Array[HM_name]  = Dp_dz
+    return Array[HM_name]
