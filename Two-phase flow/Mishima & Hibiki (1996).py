17dfdf314afdbb0b608befb100033f5d1ab4bb9c
AndrejHan <39159354+AndrejHan@users.noreply.github.com>
2019-03-26 10:52:52 +0100

    Create Mishima & Hibiki (1996).py

---
+++ Two-phase flow/Mishima & Hibiki (1996).py
@@ -0,0 +1,14 @@
+def Mishima_Hibiki_1996 (Array, Lockhart_Martinelli, D_int, liquid = True, TPM_name = 'phi^2_pred_Mishima_Hibiki_1996_l [-]'):
+    '''Calculation of Mishima and Hibiki (1996) model for two phase flow multiplier - liquid or gas.
+    Inputs are: Array - final table, Lockhart_Martinelli - L and M parameter X, D_int - internal diameter, liquid? - liquid/gas multiplier.'''    
+    import numpy as np
+    C = 21*(1-np.exp(-0.319*D_int*1e3))
+    if liquid == True: #if is phi^2 liquid
+      phi =  1 + C/Lockhart_Martinelli + 1/Lockhart_Martinelli**2
+      Array[TPM_name] = phi
+      return Array[TPM_name]
+      
+    else: #if is phi^2 gas
+        phi =  1 + C*Lockhart_Martinelli + 1*Lockhart_Martinelli**2
+        Array[TPM_name] = phi
+        return Array[TPM_name]
