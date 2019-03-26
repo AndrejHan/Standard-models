719e50e3495854df44b42eeec4a2bd34b0bf4a2f
AndrejHan <39159354+AndrejHan@users.noreply.github.com>
2019-03-26 11:38:22 +0100

    Update Fang (2011).py

--- Single phase flow/Fang (2011).py
+++ Single phase flow/Fang (2011).py
@@ -1,6 +1,6 @@
 def f_pred_Fang(Array, Reynolds, limit, f_name = 'f_pred_Fang [-]'):
     '''Calculation of predicted friction factor by Fang (2011) correlation (by Fanning) - smooth tube.
-    Inputs are: Array - final table, Reynolds - Reynolds number according to measured flow (liqud, gas, liquid-only, gas-only), limit - critical Re number.'''
+    Inputs are: Array - final table, Reynolds - Reynolds number according to measured flow (liqud, gas, liquid-only, gas-only) [-], limit - critical Re number [-].'''
     
     import numpy as np
     import pandas as pd
